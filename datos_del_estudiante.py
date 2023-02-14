9a31792c45ed465fc72efa0764bcec05dacd6303
Diego-gr-23 <124418381+Diego-gr-23@users.noreply.github.com>
2023-02-14 09:51:08 -0600

    oculte la carpeta de modulos
    agregue un * en el gitignore en la carpeta de 'modulos'.

--- datos_del_estudiante.py
+++
@@ -1,6 +0,0 @@
-# agregar los datos del estudiante
-print('Luis DIego José Gómez Ramírez')
-print('17 años de edad')
-print('Estudio ingenieria en informatica y sistemas')
-print('mi NO. de carne es: 1510223')
-print('__________________________________________________')
